5abc4eaf2467af7f440c638e5bde53d8d33b14b7
Brian Estavilla <brianestavilla@gmail.com>
2016-01-08 11:09:14 +0800

    setup navigation

--- resources/views/layout/main-css.blade.php
+++ resources/views/layout/main-css.blade.php
@@ -1,5 +1,33 @@
-<!-- Bootstrap core CSS -->
+{{-- <!-- Bootstrap core CSS -->
 <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
 <!-- Custom styles for this template -->
-<link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+<link href="{{ asset('css/dashboard.css') }}" rel="stylesheet"> --}}
+
+<!-- Tell the browser to be responsive to screen width -->
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<!-- Bootstrap 3.3.5 -->
+<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+ <link rel="stylesheet" href="{{ asset('plugins/datatables/dataTables.bootstrap.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+<!-- AdminLTE Skins. Choose a skin from the css/skins
+     folder instead of downloading all of them to reduce the load. -->
+<link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css') }}">
+<!-- iCheck -->
+<link rel="stylesheet" href="{{ asset('plugins/iCheck/flat/blue.css') }}">
+<!-- Morris chart -->
+<link rel="stylesheet" href="{{ asset('plugins/morris/morris.css') }}">
+<!-- jvectormap -->
+<link rel="stylesheet" href="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+<!-- Date Picker -->
+<link rel="stylesheet" href="{{ asset('plugins/datepicker/datepicker3.css') }}">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker-bs3.css') }}">
+<!-- bootstrap wysihtml5 - text editor -->
+<link rel="stylesheet" href="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
--- resources/views/layout/main-sidebar.blade.php
+++ resources/views/layout/main-sidebar.blade.php
@@ -1,7 +1,67 @@
-<div class="col-sm-3 col-md-2 sidebar">
-  <ul class="nav nav-sidebar">
-    @foreach($links as $link)
-      <li><a href="{{ $link['url'] }}">{{ $link['label'] }}</a></li>
-    @endforeach()
-  </ul>
-</div>
+  <!-- Left side column. contains the logo and sidebar -->
+      <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu">
+            <li class="header">MAIN NAVIGATION</li>
+            {{-- <li class="active treeview"> --}}
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                <li class="active"><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-share"></i> <span>Transactions</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li>
+                  <a href="#"><i class="fa fa-circle-o"></i> Sales <i class="fa fa-angle-left pull-right"></i></a>
+                  <ul class="treeview-menu">
+                    <li>
+                      <a href="#"><i class="fa fa-circle-o"></i> Sales Order <i class="fa fa-angle-left pull-right"></i></a>
+                      <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i> List</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i> New</a></li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-circle-o"></i> Sales Invoice <i class="fa fa-angle-left pull-right"></i></a>
+                      <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i> List</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i> New</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#"><i class="fa fa-circle-o"></i> Purchases <i class="fa fa-angle-left pull-right"></i></a>
+	                  <ul class="treeview-menu">
+		                    <li>
+		                      <a href="#"><i class="fa fa-circle-o"></i> Purchase Order<i class="fa fa-angle-left pull-right"></i></a>
+		                      <ul class="treeview-menu">
+		                        <li><a href="#"><i class="fa fa-circle-o"></i> List</a></li>
+		                        <li><a href="#"><i class="fa fa-circle-o"></i> New</a></li>
+		                      </ul>
+		                    </li>
+		                    <li>
+		                      <a href="#"><i class="fa fa-circle-o"></i> Purchase Requisition<i class="fa fa-angle-left pull-right"></i></a>
+		                      <ul class="treeview-menu">
+		                        <li><a href="#"><i class="fa fa-circle-o"></i> List</a></li>
+		                        <li><a href="#"><i class="fa fa-circle-o"></i> New</a></li>
+		                      </ul>
+		                    </li>
+	                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
